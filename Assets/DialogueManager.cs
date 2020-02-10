27523a4358268e41e1dddc51f26e34f62fd45b9c
Cosmin Blaga <cosmin.blaga@kepler-rominfo.com>
2020-02-10 15:32:04 +0200

    Fix words jumping on next line

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -48,6 +48,12 @@ public class DialogueManager : MonoBehaviour
 
     IEnumerator TypeSentence(string sentence)
     {
+        dialogueText.text = sentence;
+        Canvas.ForceUpdateCanvases();
+        dialogueText.text = string.Empty;
+        
+        sentence = GetFormattedText(dialogueText, sentence);
+        // Debug.Log(sentence);
         dialogueText.text = "";
         foreach (char letter in sentence.ToCharArray())
         {
@@ -55,6 +61,51 @@ public class DialogueManager : MonoBehaviour
             yield return null;
         }
     }
+
+    private string GetFormattedText (Text dialogueText, string sentence) {
+         string[] words = sentence.Split(' ');
+ 
+         int width = Mathf.FloorToInt(dialogueText.rectTransform.sizeDelta.x);
+        //  Debug.Log("width: " + width);
+         int space = GetWordSize(" ", dialogueText.font, dialogueText.fontSize);
+        //  Debug.Log("space: " + space);
+ 
+         string newText = string.Empty;
+         int count = 0;
+         for (int i = 0; i < words.Length; i++) {
+             int size = GetWordSize(words[i], dialogueText.font, dialogueText.fontSize);
+            //  Debug.Log("word size: " + size);
+ 
+             if (i == 0) {
+                 newText += words[i];
+                 count += size;
+             }
+             else if (count + space > width || count + space + size > width) {
+                 newText += "\n";
+                 newText += words[i];
+                 count = size;
+             }
+             else if (count + space + size <= width) {
+                 newText += " " + words[i];
+                 count += space + size;
+             }
+         }
+        //  Debug.Log("text after format: " + newText);
+         return newText;
+     }
+ 
+     private int GetWordSize (string word, Font font, int fontSize) {
+         char[] arr = word.ToCharArray();
+        //  Debug.Log("word char length: " + arr.Length);
+         CharacterInfo info;
+         int size = 0;
+         for (int i = 0; i < arr.Length; i++) {
+             font.GetCharacterInfo(arr[i], out info, fontSize);
+            //  Debug.Log("info.advance: " + info.advance);
+             size += info.advance;
+         }
+         return size;
+     }
 
     public void EndDialogue()
     {
@@ -65,7 +116,7 @@ public class DialogueManager : MonoBehaviour
             return;
         } else
         {
-            Debug.Log("End of dialogue.");
+            // Debug.Log("End of dialogue.");
             animator.SetBool("isOpen", false);
         }
     }
